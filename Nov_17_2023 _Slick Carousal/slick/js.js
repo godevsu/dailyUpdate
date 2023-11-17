805590da8e04f4ab6f948a334a139dcd3abda77e
Gokulanadh Vagvala <gokulanadh.dev@gmail.com>
2023-11-17 17:01:39 +0530

    first commit

---
+++ Nov_17_2023 _Slick Carousal/slick/js.js
@@ -0,0 +1,8 @@
+
+
+$(document).ready(() => {
+    $('.carousel').slick({
+        autoplay: true,
+        
+    });
+  });
---
+++ Nov_17_2023 _Slick Carousal/slick_lamba/js.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+    $('.carousel').slick({
+        autoplay:false,
+        autoplaySpeed: 1000,
+        fade: true,
+        slide: 'div',
+        speed: 1000,
+        prevArrow: '.arrow_prev',
+        nextArrow: '.arrow_next'
+    });
+  });
+
+
+ 
